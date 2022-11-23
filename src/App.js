91e146b9cd7a89e041b438c6940eaf8dcf166211
easymanik <easymanik151199@gmail.com>
2022-11-23 21:57:24 +0700

    gabung sama auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
-import LandingPage from './page/LandingPage/LandingPage';
-import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import "./App.css";
+//ridwhan
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+//easy
+import LoginPekerja from "./pages/auth/pekerja/login";
+import RegisterPekerja from "./pages/auth/pekerja/register";
+import RegisterPerekrut from "./pages/auth/perekrut/register";
+import LoginPerekrut from "./pages/auth/perekrut/login";
+import ResetPw from "./pages/auth/resetpw";
+import ResetPw2 from "./pages/auth/resetpw2";
+import LoginResetpwDone from "./pages/auth/loginResetpwDone";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/landingPage"/>} replace="true"/>
-            <Route path="/landingPage" element={<LandingPage />} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/landingPage" />}
+            replace="true"
+          />
+          <Route path="/landingPage" element={<LandingPage />} />
+
+          <Route path="/loginPekerja" element={<LoginPekerja />} />
+          <Route path="/registerPekerja" element={<RegisterPekerja />} />
+          <Route path="/registerPerekrut" element={<RegisterPerekrut />} />
+          <Route path="/loginPerekrut" element={<LoginPerekrut />} />
+          <Route path="/resetPw" element={<ResetPw />} />
+          <Route path="/resetPw2" element={<ResetPw2 />} />
+          <Route path="/loginResetpwDone" element={<LoginResetpwDone />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
